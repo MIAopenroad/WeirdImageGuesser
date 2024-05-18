fc8c6963de32471a9c8acc777eacc1277a636957
Kei Minagawa <minagawa@fun.bio.keio.ac.jp>
2024-05-18 22:19:26 +0900

    update generate_prompt, generate_img_from_prompt, game_start

--- backend/gameStart.py
+++ backend/gameStart.py
@@ -10,31 +10,42 @@ client = openai.OpenAI()
 
 # GPT-3.5によって絵のお題のpromptを生成する関数
 def generate_prompt():
-    response = client.completions.create(
-        model="gpt-3.5-turbo-0125",
-        prompt="""
-            変な絵を出力するために以下の条件を満たす日本語のpromptを作成してください。
-            #dalle3のAPIに直接渡されるため、prompt以外の情報は含めない
-            #promptは一つ
-            #短く、推測されにくい内容かつ日本語
-            #""で囲む"
-            """
-    )
-    print(response["choices"][0]["text"])
-    img_prompt = response["choices"][0]["text"]
-    return img_prompt
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {"role": "system", "content": "あなたはプロンプトを生成するアシスタントです。"},
+                {"role": "user", "content": """
+                                変な絵を出力するために以下の条件を満たす日本語のpromptを作成してください。
+                                #dalle3のAPIに直接渡されるため、prompt以外の情報は含めない
+                                #promptは一つ
+                                #短く、推測されにくい内容かつ日本語
+                                #""で囲む
+                                """
+                 },
+            ]
+        )
+        img_prompt = response.choices[0].message.content
+        return img_prompt
+    except openai.OpenAIError as e:
+        print(f"Error: {e}")
+        return None
 
 
 # プロンプトから画像を生成してURLを返す関数
 def generate_img_from_prompt(img_prompt: str):
-    response = client.images.generate(
-        model="dall-e-3",
-        prompt=img_prompt,
-        size="1024x1792",
-        quality="standard",
-        n=1,
-    )
-    return response.data[0].url
+    try:
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=img_prompt,
+            size="1792x1024",
+            quality="standard",
+            n=1,
+        )
+        return response.data[0].url
+    except openai.OpenAIError as e:
+        print(f"Error: {e}")
+        return None
 
 
 # 画像生成、URLの出力を行うエンドポイント
@@ -42,9 +53,12 @@ def generate_img_from_prompt(img_prompt: str):
 def game_start():
     img_prompt = generate_prompt()
     url = generate_img_from_prompt(img_prompt)
-    print(url)
-    return url
+    return url, img_prompt
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # img_pr = generate_prompt()
+    # print(img_pr)
+    # url = generate_img_from_prompt("cat")
+    # print(url)
