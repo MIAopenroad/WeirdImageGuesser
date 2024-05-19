36f235e9136c2c81f38d83364a0ffa8765bf0dba
Taichi Araki <araki130710@gmail.com>
2024-05-19 13:11:44 +0900

    add axiosClient and some endpoint functions.

--- frontend/src/api/functions.ts
+++ frontend/src/api/functions.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
 import { QUESTION_URI, SCORE_URI } from "../App";
 import { QuestionResponse, ScoreResponse } from "../types";
+import { axiosClient } from "../providers/axiosClient";
 
 export const getQuestion = async (): Promise<QuestionResponse> => {
-  const { data } = await axios.get(QUESTION_URI);
+  const { data } = await axiosClient.get(QUESTION_URI);
   return {
     imageURL: data.url,
     answer: data.prompt,
@@ -14,7 +14,7 @@ export const gradeAnswers = async (
   answers: string[],
   answerPrompt: string
 ): Promise<ScoreResponse> => {
-  const { data } = await axios.post(SCORE_URI, { answers, answerPrompt });
+  const { data } = await axiosClient.post(SCORE_URI, { answers, answerPrompt });
   return {
     scores: data.scores,
   };
