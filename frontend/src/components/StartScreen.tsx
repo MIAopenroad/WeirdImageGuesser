beaf8a268d7b6f2e9dad0a330de08a4d203d0c53
imada kakeru <imada@fun.bio.keio.ac.jp>
2024-05-19 13:05:42 +0900

    create form startScreen

--- frontend/src/components/StartScreen.tsx
+++ frontend/src/components/StartScreen.tsx
@@ -1,4 +1,3 @@
-// components/StartScreen.tsx
 import React, { useState } from 'react';
 import { Participant } from '../types';
 import { APP_TITLE } from '../consts';
@@ -9,6 +8,13 @@ import {
   Input,
   Stack,
   Heading,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  SliderMark,
+  Grid,
+  GridItem
 } from '@chakra-ui/react';
 
 interface StartScreenProps {
@@ -18,29 +24,105 @@ interface StartScreenProps {
 const StartScreen: React.FC<StartScreenProps> = ({ startGame }) => {
   const [numParticipants, setNumParticipants] = useState(0);
   const [rounds, setRounds] = useState(0);
+  const [playersName, setPlayersName] = useState(Array(numParticipants).fill(''));
+
+  const handlePlayersName = (name: string,index: number) =>{
+    const newPlayersName = [...playersName];
+    newPlayersName[index] = name;
+    setPlayersName(newPlayersName);
+  }
 
   const handleStart = () => {
     const participants = Array.from({ length: numParticipants }, (_, index) => ({
-      name: `Player ${index + 1}`,
+      name: playersName[index],
       score: 0
     }));
     startGame(participants, rounds);
   };
 
+  const handleParticipantChange = (value: number) =>{
+    setNumParticipants(value);
+  }
+
+  const handleRoundsChange = (value: number) => {
+    setRounds(value);
+  }
+
   return (
     <>
-      <Heading as="h1" size="xl" mb={6}>
-        {APP_TITLE}
-      </Heading>
+      <div style={{position: 'absolute', top: 40, left: 0, width: '100%', textAlign: 'center'}}>
+        <Heading as="h1" size="xl" mb={6} mt={0}>
+          {APP_TITLE}
+        </Heading>
+      </div>
+
       <Stack spacing={4} as="form" onSubmit={e => e.preventDefault()}>
         <FormControl id="numParticipants">
           <FormLabel>Number of Participants</FormLabel>
-          <Input type="number" value={numParticipants} onChange={e => setNumParticipants(parseInt(e.target.value))} />
+          <Slider 
+            aria-label='numParticipants=slider'
+            style={{ width: '80%' }}
+            colorScheme={'pink'}
+            min={1}
+            max={4}
+            value={numParticipants}
+            onChange={handleParticipantChange}
+          >
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            {Array.from({ length: 4 }, (_, index) => (
+              <SliderMark key={index + 1} value={index + 1} mt='1' ml='-2.5' fontSize='sm'>
+                {index + 1}
+              </SliderMark>
+            ))}
+            <SliderThumb boxSize={6} />
+          </Slider>
         </FormControl>
+
         <FormControl id="rounds">
           <FormLabel>Number of Rounds</FormLabel>
-          <Input type="number" value={rounds} onChange={e => setRounds(parseInt(e.target.value))} />
+          <Slider aria-label='rounds-slider' min={1} max={10} value={rounds} onChange={handleRoundsChange}>
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            {Array.from({ length: 10 }, (_, index) => (
+              <SliderMark key={index + 1} value={index + 1} mt='1' ml='-2.5' fontSize='sm'>
+                {index + 1}
+              </SliderMark>
+            ))}
+            <SliderThumb boxSize={6} />
+          </Slider>
         </FormControl>
+
+        <Grid templateColumns="repeat(2, 1fr)" gap={2}>
+          {Array.from({ length: numParticipants }).map((_, index) => (
+            <GridItem key={`player-${index + 1}`} colSpan={1} rowSpan={1}>
+              <Stack spacing={4} style={{ position: 'relative', width: '100%' }}>
+                <FormControl id={`player-${index}`}>
+                  <FormLabel>{`Player ${index + 1} name`}</FormLabel>
+                  <Input 
+                  type='text'
+                  onChange={e=> handlePlayersName(e.target.value, index)} />
+                </FormControl>
+              </Stack>
+            </GridItem>
+          ))}
+          {Array.from({ length: 4 - numParticipants }).map((_, index) => (
+            <GridItem key={`player-${ numParticipants -index}`} colSpan={1} rowSpan={1}>
+              <Stack spacing={4} style={{ position: 'relative', width: '100%' }}>
+                <FormControl id={`player-${index}`}>
+                  <FormLabel>{`Player ${index + 1} name`}</FormLabel>
+                  <Input 
+                  type='text'
+                  disabled
+                  onChange={e=> handlePlayersName(e.target.value, index)} />
+                </FormControl>
+              </Stack>
+            </GridItem>
+          ))}
+        </Grid>
+
         <Button colorScheme="teal" onClick={handleStart}>
           Start Game
         </Button>
