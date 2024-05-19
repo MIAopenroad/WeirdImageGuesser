899185fe090261bbf7d9d5348c5bb691dcde91b9
Taichi Araki <araki130710@gmail.com>
2024-05-19 13:15:45 +0900

    add cors setting

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 
 if __name__ == "__main__":
     app = Flask(__name__)
+    CORS(app, resources={"/": {"origins": "http://localhost:5173"}})
     app.run(debug=True)
