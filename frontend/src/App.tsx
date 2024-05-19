1439da0b2695350d3890e8ad022942ad4d51713e
Taichi Araki <araki130710@gmail.com>
2024-05-19 12:21:12 +0900

    Add uris.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,92 +1,75 @@
 // App.tsx
-import React, { useState } from 'react';
-import { GameState, Participant, ScreenKind } from './types';
-import StartScreen from './components/StartScreen';
-import GameScreen from './components/GameScreen';
-import ResultsScreen from './components/ResultsScreen';
-import { Box, Center } from '@chakra-ui/react';
+import React, { useState } from "react";
+import { GameState, Participant, ScreenKind } from "./types";
+import StartScreen from "./components/StartScreen";
+import GameScreen from "./components/GameScreen";
+import ResultsScreen from "./components/ResultsScreen";
+import { Box, Center } from "@chakra-ui/react";
+
+export const QUESTION_URI = "http://localhost:5000/gameStart";
+export const SCORE_URI = "http://localhost:5000/calcScore";
 
 const initialGameState: GameState = {
   participants: [],
   rounds: 0,
   currentRound: 0,
   currentScreen: ScreenType.Start,
-  imageURL: '',
-  answerPrompt: ''
+  imageURL: "",
+  answerPrompt: "",
 };
 
 const App: React.FC = () => {
-<<<<<<< Updated upstream
-  const [gameState, setGameState] = useState<GameState>({
-    participants: [],
-    rounds: 0,
-    currentRound: 0,
-    currentScreen: ScreenKind.Start
-  });
-=======
   const [gameState, setGameState] = useState<GameState>(initialGameState);
->>>>>>> Stashed changes
 
   const startGame = (participants: Participant[], rounds: number) => {
-    imageURL, answer = getQuestion();
+    imageURL, (answer = getQuestion());
     setGameState({
       participants: participants,
       rounds: rounds,
       currentRound: 1,
-<<<<<<< Updated upstream
-      currentScreen: ScreenKind.Game
-=======
       currentScreen: ScreenType.Game,
       imageURL: imageURL,
-      answerPrompt: answer
->>>>>>> Stashed changes
+      answerPrompt: answer,
     });
   };
 
   const nextRound = (newParticipants: Participant[]) => {
     if (gameState.currentRound < gameState.rounds) {
-      imageURL, answer = getQuestion();
-      setGameState(prevState => ({
+      imageURL, (answer = getQuestion());
+      setGameState((prevState) => ({
         ...prevState,
         participants: newParticipants,
         currentRound: prevState.currentRound + 1,
         imageURL: imageURL,
-        answerPrompt: answer
+        answerPrompt: answer,
       }));
     } else {
-      setGameState(prevState => ({
+      setGameState((prevState) => ({
         ...prevState,
         participants: newParticipants,
-<<<<<<< Updated upstream
-        currentScreen: ScreenKind.Results
-=======
         currentScreen: ScreenType.Results,
-        imageURL: '',
-        answerPrompt: ''
->>>>>>> Stashed changes
+        imageURL: "",
+        answerPrompt: "",
       }));
     }
   };
 
   const returnToStart = () => {
-<<<<<<< Updated upstream
-    setGameState({
-      participants: [],
-      rounds: 0,
-      currentRound: 0,
-      currentScreen: ScreenKind.Start
-    });
-=======
     setGameState(initialGameState);
->>>>>>> Stashed changes
   };
 
   return (
     <Center minHeight="100vh" minWidth="100vw">
       <Box textAlign="center" py={10} px={6}>
-        {gameState.currentScreen === ScreenKind.Start && <StartScreen startGame={startGame} />}
-        {gameState.currentScreen === ScreenKind.Game && <GameScreen gameState={gameState} nextRound={nextRound} />}
-        {gameState.currentScreen === ScreenKind.Results && <ResultsScreen gameState={gameState} returnToStart={returnToStart} />}
+        {gameState.currentScreen === ScreenKind.Start && (
+          <StartScreen startGame={startGame} />
+        )}
+        {gameState.currentScreen === ScreenKind.Game && (
+          <GameScreen gameState={gameState} nextRound={nextRound} />
+        )}
+        {gameState.currentScreen === ScreenKind.Results && (
+          <ResultsScreen gameState={gameState} returnToStart={returnToStart} />
+        )}
       </Box>
     </Center>
   );
